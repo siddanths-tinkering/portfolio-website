49ecbce30aa0eaa9ca81eb66229c3568a6099d62
Siddanth <xpssiddanthratan@gmail.com>
2023-08-04 21:24:52 +0800

    Fix blank links and highlights

--- my-app/src/components/review.js
+++ my-app/src/components/review.js
@@ -6,21 +6,19 @@ import { faLink } from '@fortawesome/free-solid-svg-icons';
 function Review({title, sentence, link, video}) {
 	return (
 		<div>
-			<a href={link}>
-				<div className="container">
-					<div className="review-container-1">
-						<div className="review-container-2">
-							<b className="title">{title} </b>
-							<a><FontAwesomeIcon className='icon' icon={faLink} /></a>
-							<p className="sentence">{sentence}</p>
-						</div>
-						<b className="rating">{}</b>
-						<div className="image">
-							<img src={video} id="GIF"></img>
-						</div>
+			<div className="container">
+				<div className="review-container-1">
+					<div className="review-container-2">
+						<b className="title">{title} </b>
+						<a href={link}><FontAwesomeIcon className='icon' icon={faLink} /></a>
+						<p className="sentence">{sentence}</p>
+					</div>
+					<b className="rating">{}</b>
+					<div className="image">
+						<img src={video} id="GIF"></img>
 					</div>
 				</div>
-			</a>
+			</div>
 		</div>
 	)
 }
--- my-app/src/components/role.js
+++ my-app/src/components/role.js
@@ -6,26 +6,24 @@ import { faLink	} from '@fortawesome/free-solid-svg-icons'
 function Role({company, role, description, dates, link="", country="Singapore"}) {
 	return (
 		<div>
-			<a href={link}>
-				<div className="container">
-					<div className="container-2">
-						<div className="container-3">
-							<b>{role} </b>
-							{link == "" ? "" : <a><FontAwesomeIcon className="icon" icon={faLink} /></a>}<br/>
-							<b className="company">{company}</b>
-						</div>
-						<div className='container-4'>
-							<b className="dates">{dates}</b><br/>
-							<b className="company">{country}</b><br/>
-						</div>
+			<div className="container">
+				<div className="container-2">
+					<div className="container-3">
+						<b>{role} </b>
+						{link == "" ? "" : <a href={link}><FontAwesomeIcon className="icon" icon={faLink} /></a>}<br/>
+						<b className="company">{company}</b>
 					</div>
-					<React.Fragment>
-						<ul>
-							{description.map((text) => (<li className="text">{text}</li>))}
-						</ul>
-					</React.Fragment>
+					<div className='container-4'>
+						<b className="dates">{dates}</b><br/>
+						<b className="company">{country}</b><br/>
+					</div>
 				</div>
-			</a>
+				<React.Fragment>
+					<ul>
+						{description.map((text) => (<li className="text">{text}</li>))}
+					</ul>
+				</React.Fragment>
+			</div>
 		</div>
 	)
 }
