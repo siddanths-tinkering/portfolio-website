913252fd5601e6ff1a28045ab323b60548f30ed2
Siddanth <xpssiddanthratan@gmail.com>
2023-08-04 21:14:01 +0800

    Fix merge conflicts

--- my-app/src/components/review.js
+++ my-app/src/components/review.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import '../styles/review.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
-function Review({title, sentence, rating, video}) {
+function Review({title, sentence, link, video}) {
 	return (
 		<div>
 			<div className="container">
 				<div className="review-container-1">
 					<div className="review-container-2">
-						<b className="title">{title}</b>
+						<b className="title">{title} </b>
+						<a href={link}><FontAwesomeIcon className='icon' icon={faLink} /></a>
 						<p className="sentence">{sentence}</p>
 					</div>
-					<b className="rating">{rating}</b>
-					<img src={video} id="GIF"></img>
+					<b className="rating">{}</b>
+					<div className="image">
+						<img src={video} id="GIF"></img>
+					</div>
 				</div>
 			</div>
 		</div>
